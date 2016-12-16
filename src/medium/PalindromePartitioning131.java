31d15d15ba8f7689de82647480fbefb0d763c521
ShiyunHUANG <huangshiyun44@hotmail.com>
2016-12-15 23:51:23 -0500

    backtrack

--- src/medium/PalindromePartitioning131.java
+++ src/medium/PalindromePartitioning131.java
@@ -6,6 +6,27 @@ import java.util.List;
 public class PalindromePartitioning131 {
     List<List<String>> res = new ArrayList<List<String>>();
     public List<List<String>> partition(String s) {
+        dfs(s, new ArrayList<String>());
+        return res;
+    }
+    private void dfs(String str, List<String> path) {
+        //terminate condition
+        if(str.length() == 0){//
+          res.add(new ArrayList<String>(path));
+          return;  
+        }
+        //loop and recurrsion
+        for(int i = 1; i <= str.length(); i++) {
+            String sub = str.substring(0, i);
+            if(isPalindrome(sub)) {
+                path.add(sub);
+                dfs(str.substring(i), path);
+                path.remove(path.size() - 1);//remove if just added   
+            }
+        }
+    }
+    /*List<List<String>> res = new ArrayList<List<String>>();
+    public List<List<String>> partition(String s) {
         dfs(s, 0, 1, new ArrayList<String>(), false);
         return res;
     }
@@ -36,7 +57,7 @@ public class PalindromePartitioning131 {
             System.out.println("removed " + path);
         }
         dfs(str, start, end + 1, path, true);
-    }
+    }*/
     private boolean isPalindrome(String str) {
         int p1 = 0, p2 = str.length() - 1;
         while(p1 <= p2) {
